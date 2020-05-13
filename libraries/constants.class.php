1a93140281d4354263c08ec7c4e3d3152477b87b
dpalade06-alt <65154904+dpalade06-alt@users.noreply.github.com>
2020-05-13 11:58:00 +0200

    Update constants.class.php

--- libraries/constants.class.php
+++ libraries/constants.class.php
@@ -4,7 +4,7 @@
 define("DB_HOST", "localhost");
 define("DB_DATABASE", "poules4all");
 define("DB_USERNAME", "poules4all_user");
-define("DB_PASSWORD", "$3s5Tkb0");
+define("DB_PASSWORD", "wachtwoord");
 
 define("SENDER_EMAIL", "76431@glr.nl");
 
